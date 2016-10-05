31480f3e4878fc75c771788f77069237fde75ede
Ankit Shah <ank.h.shah@gmail.com>
2016-10-05 14:37:21 -0400

    Multiply and divide

--- test_rpn.py
+++ test_rpn.py
@@ -14,4 +14,12 @@ class TestBasics(unittest.TestCase):
     def test_bad_string(self):
         with self.assertRaises(TypeError):
             rpn.calculate("1 2 3 +")
+
+    def test_multiply(self):
+        result = rpn.calculate("3 7 *")
+        self.assertEqual(21, result)
+
+    def test_divide(self):
+        result = rpn.calculate("6 3 /")
+        self.assertEqual(2, result)
         
